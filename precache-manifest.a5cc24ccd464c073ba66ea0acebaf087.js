f09bf68b5d069a2fac169bff8e0deae964f041b5
ibrahim mohammed <ibrahimbmohammed@gamil.com>
2019-08-28 11:38:49 +0100

    Updates

---
+++ precache-manifest.a5cc24ccd464c073ba66ea0acebaf087.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "74b319ba0e4ab22bb15761a8add5054e",
+    "url": "/ibrahimbmohammed/react-phone-store/index.html"
+  },
+  {
+    "revision": "238efd07790f40db3f6e",
+    "url": "/ibrahimbmohammed/react-phone-store/static/css/main.84335fd2.chunk.css"
+  },
+  {
+    "revision": "db6795955b71b4e6ea8d",
+    "url": "/ibrahimbmohammed/react-phone-store/static/js/2.2cf23b2f.chunk.js"
+  },
+  {
+    "revision": "238efd07790f40db3f6e",
+    "url": "/ibrahimbmohammed/react-phone-store/static/js/main.a16db307.chunk.js"
+  },
+  {
+    "revision": "69d60d796b265611152b",
+    "url": "/ibrahimbmohammed/react-phone-store/static/js/runtime~main.85adc092.js"
+  }
+]);
